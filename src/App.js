68e07593a19dcf583bcc75ef36fba512f7242fc9
Sylvia Makuch <sylvia@trimboli.io>
2023-03-03 01:38:05 -0700

    style changes to app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const Background = styled.div`
-    background-color: #1fa6a8;
+    background: rgb(31, 166, 168);
     height: 100vh;
     width: 100vw;
     display: flex;
@@ -27,9 +27,8 @@ const Card = styled.div`
     align-items: center;
     flex-direction: column;
     border-radius: 67px;
-    background: #1fa6a8;
-    box-shadow:  41px -41px 52px #1b9294,
-                -41px 41px 52px #23babc;
+    background: rgb(31, 166, 168);
+    box-shadow: rgb(27 146 148) 41px -41px 52px, rgb(35 186 188) -41px 41px 52px;
 `;
 
 const Input = styled.input`
@@ -38,8 +37,8 @@ const Input = styled.input`
     border-radius: 67px;
     text-align: center;
     boarder: none;
-    border-radius: 67px;
-    background: rgb(39 193 195);
+    border-radius: 15px;
+    background: rgb(137 191 201);
     border-style: hidden;
     /* box-shadow:  41px -41px 52px #1b9294,
              -41px 41px 52px #619d9d; */

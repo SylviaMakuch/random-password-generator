a28582cb9987bd21f4c3cd107bf5faed4a2c89d7
Sylvia Makuch <sylvia@trimboli.io>
2023-03-03 21:00:54 -0700

    styling complete

--- src/App.js
+++ src/App.js
@@ -2,57 +2,127 @@ import React from "react";
 import styled from "styled-components";
 
 const Background = styled.div`
-    background: rgb(31, 166, 168);
-    height: 100vh;
-    width: 100vw;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
+  background: rgb(31, 166, 168);
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
 
 const H1 = styled.h1`
-    color: #fff;
-    font-size: 3rem;
-    font-weight: 700;
-    margin-bottom: 10%;
+  color: #fff;
+  font-size: 3rem;
+  font-family: "Roboto Mono", monospace;
+  font-weight: 700;
+  margin-bottom: 10%;
 `;
 
 const Card = styled.div`
-    height: 800px;
-    width: 600px;
-    padding: 1.5rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    border-radius: 67px;
-    background: rgb(31, 166, 168);
-    box-shadow: rgb(27 146 148) 41px -41px 52px, rgb(35 186 188) -41px 41px 52px;
+  padding: 3rem;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-evenly;
+  border-radius: 67px;
+  background: rgb(31, 166, 168);
+  box-shadow: rgb(27 146 148) 41px -41px 52px, rgb(35 186 188) -41px 41px 52px;
 `;
 
 const Input = styled.input`
-    height: 50px;
-    width: 400px;
-    border-radius: 67px;
-    text-align: center;
-    boarder: none;
-    border-radius: 15px;
-    background: rgb(137 191 201);
-    border-style: hidden;
-    /* box-shadow:  41px -41px 52px #1b9294,
+  height: 50px;
+  width: 400px;
+  border-radius: 67px;
+  font-family: "Roboto Mono", monospace;
+  text-align: center;
+  boarder: none;
+  border-radius: 15px;
+  background: #06cfc185;
+  border-style: hidden;
+  /* box-shadow:  41px -41px 52px #1b9294,
              -41px 41px 52px #619d9d; */
 `;
 
+const RadioContainer = styled.div`
+  margin: 2rem;
+  width: 16rem;
+`;
 
+const RadioInputContainer = styled.div`
+  margin: 5% 0 4% 0;
+  width: 16rem;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Radio = styled.input`
+  box-sizing: border-box;
+  appearance: none;
+  background: white;
+  outline: 2px solid #333;
+  border: 3px solid white;
+  width: 0.8rem;
+  height: 0.8rem;
+`;
+
+const Button = styled.button`
+  height: 2rem;
+  width: 12rem;
+  border-radius: 67px;
+  font-family: "Roboto Mono", monospace;
+  text-align: center;
+  boarder: none;
+  border-radius: 15px;
+  border-radius: 50px;
+  background: linear-gradient(225deg, #06cfc1, #05aea2);
+  box-shadow: -1px 1px 3px #024d48, 1px -1px 3px #0affff;
+  border-style: hidden;
+`;
+
+const Label = styled.label`
+  color: black;
+  font-family: "Roboto Mono", monospace;
+  font-size: 1rem;
+  font-weight: 500;
+`;
 
 function App() {
     return (
         <Background>
             <H1> Random Password Generator</H1>
             <Card>
-                <Input type="text" placeholder="Password" />
-
+                <Input type="text" placeholder="Your Password" />
+                <RadioContainer>
+                    <RadioInputContainer>
+                    <Label for="length">Password Length :</Label>
+                    <input
+                        value="6"
+                        name="length"
+                        step="1"
+                        type="number"
+                        min="6"
+                        max="40"
+                    />
+                    </RadioInputContainer>
+                    <RadioInputContainer>
+                        <Label>Upper Case</Label>
+                        <Radio type="radio" name="upperCase" />
+                    </RadioInputContainer>
+                    <RadioInputContainer>
+                        <Label>Lower Case</Label>
+                        <Radio type="radio" name="lowerCase" />
+                    </RadioInputContainer>
+                    <RadioInputContainer>
+                        <Label>Numbers</Label>
+                        <Radio type="radio" name="numbers" />
+                    </RadioInputContainer>
+                    <RadioInputContainer>
+                        <Label>Numbers</Label>
+                        <Radio type="radio" name="symbols" />
+                    </RadioInputContainer>
+                </RadioContainer>
+                <Button>Generate</Button>
             </Card>
         </Background>
     );

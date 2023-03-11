d5d4aa850397aeecc1db07b103b7434bfad7fe3e
Sylvia Makuch <sylvia@trimboli.io>
2023-03-11 00:19:51 -0700

    styling changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import { radioOptions } from "./config";
 
 const Background = styled.div`
-  background: rgb(31, 166, 168);
+  background: rgb(22 188 191);
   height: 100vh;
   width: 100vw;
   display: flex;
@@ -19,30 +19,43 @@ const H1 = styled.h1`
   font-family: "Roboto Mono", monospace;
   font-weight: 700;
   margin-bottom: 10%;
+  text-align: center;
+
+  @media (max -width: 1024px) {
+    font-size: 2rem;
+  }
 `;
 
 const Card = styled.div`
-  padding: 3rem;
+  padding: 2rem;
+  width: 40%;
   display: flex;
   align-items: center;
   flex-direction: column;
   justify-content: space-evenly;
   border-radius: 67px;
-  background: rgb(31, 166, 168);
+  background: #20c2c4c4;
   box-shadow: rgb(27 146 148) 41px -41px 52px, rgb(35 186 188) -41px 41px 52px;
+
+  @media (max-width: 1024px) {
+    width: 80%;
+  }
 `;
 
 const Input = styled.input`
   height: 50px;
   width: 400px;
+  margin: 2rem;
   border-radius: 67px;
   font-family: "Roboto Mono", monospace;
   text-align: center;
-  boarder: none;
+  font-size: 1.5rem;
   border-radius: 15px;
   background: #06cfc185;
   border-style: groove;
   border-color: #f7feff36;
+  height: 3rem;
+  width: 78%;
   &:focus {
     outline: none;
   }
@@ -50,25 +63,27 @@ const Input = styled.input`
 
 const CheckBoxContainer = styled.div`
   margin: 2rem;
-  width: 16rem;
+  width: 75%;
 `;
 
 const CheckBoxInputContainer = styled.div`
   display: grid;
-  grid-template-columns: 80% 20%;
+  grid-template-columns: 90% 10%;
   grid-template-rows: 1fr;
   gap: 0px 0px;
   grid-template-areas: ". .";
   justify-content: space-between;
-  margin: 15px 0;
+  margin: 1rem 0;
 `;
 
 const Button = styled.button`
-  height: 2rem;
-  width: 12rem;
+  height: 3rem;
+  padding: 1rem 3.2rem;
+  margin: 2rem;
   border-radius: 67px;
   font-family: "Roboto Mono", monospace;
   text-align: center;
+  font-size: 1rem;
   border-radius: 15px;
   border-radius: 50px;
   background: linear-gradient(120deg, #06cfc1, #05aea2);
@@ -87,8 +102,11 @@ const Button = styled.button`
 const Label = styled.label`
   color: black;
   font-family: "Roboto Mono", monospace;
-  font-size: 1rem;
+  font-size: 1.5rem;
   font-weight: 500;
+  @media (max-width: 768px) {
+    font-size: 2rem;
+  }
 `;
 
 function App() {
@@ -124,15 +142,18 @@ function App() {
                 name={option.value}
                 value={option.value}
                 min={option.min}
-                max={option.max} x
+                max={option.max}
+                x
                 key={index}
                 onChange={() => {
-                  const newCheckbox = [...checkbox];//copy the array
-                  if (newCheckbox.includes(option.value)) { //if the array includes the value
+                  const newCheckbox = [...checkbox]; //copy the array
+                  if (newCheckbox.includes(option.value)) {
+                    //if the array includes the value
                     const index = newCheckbox.indexOf(option.value); //get the index of the value
                     newCheckbox.splice(index, 1); //remove the value from the array
                   }
-                  if (!newCheckbox.includes(option.value)) { //if the array does not include the value
+                  if (!newCheckbox.includes(option.value)) {
+                    //if the array does not include the value
                     newCheckbox.push(option.value); //add the value to the array
                   }
                   setCheckbox(newCheckbox);
@@ -141,33 +162,34 @@ function App() {
             </CheckBoxInputContainer>
           ))}
         </CheckBoxContainer>
-        <Button onClick={() => {
-          const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-          const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-          const numbers = "0123456789";
-          const symbols = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
-          let output = ""; //create an empty string
+        <Button
+          onClick={() => {
+            const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+            const numbers = "0123456789";
+            const symbols = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+            let output = ""; //create an empty string
 
-          for (let i = 0; i < length; i++) { //loop through the length of the password
-            if (checkbox.includes("upperCase")) { //if the checkbox includes uppercase
-              output += upperCase[Math.floor(Math.random() * upperCase.length)] //add a random uppercase letter to the output string
-
+            for (let i = 0; i < length; i++) {
+              //loop through the length of the password
+              if (checkbox.includes("upperCase")) {
+                //if the checkbox includes uppercase
+                output +=
+                  upperCase[Math.floor(Math.random() * upperCase.length)]; //add a random uppercase letter to the output string
+              }
+              if (checkbox.includes("lowerCase")) {
+                output +=
+                  lowerCase[Math.floor(Math.random() * lowerCase.length)]; //
+              }
+              if (checkbox.includes("numbers")) {
+              }
+              if (checkbox.includes("symbols")) {
+                output += symbols[Math.floor(Math.random() * symbols.length)];
+              }
+              let pw = output.slice(0, length); //slice the output string to the length of the password
+              setPassword(pw); //set the password state to the output string
             }
-            if (checkbox.includes("lowerCase")) {
-              output += lowerCase[Math.floor(Math.random() * lowerCase.length)] //
-
-            }
-            if (checkbox.includes("numbers")) {
-
-            }
-            if (checkbox.includes("symbols")) {
-              output += symbols[Math.floor(Math.random() * symbols.length)]
-
-            }
-            let pw = output.slice(0, length); //slice the output string to the length of the password
-            setPassword(pw); //set the password state to the output string
-          }
-        }}
+          }}
         >
           Generate Password
         </Button>
